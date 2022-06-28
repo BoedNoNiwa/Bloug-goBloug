aa5cbe43cdd542eb55a37070a95319db9675e3a8
BoedNoNiwa <dukunas27@gmail.com>
2022-06-28 16:47:20 +0700

    About Page Responsive Fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About({ author }) {
   const padSize = useBreakpointValue({base: "50", md: "40", sm: "10"})
 
   return (
-    <Box width="5xl" paddingX={padSize} marginY="24" textAlign="left" alignSelf="start" _dark={{color: "white"}}>
+    <Box maxWidth="5xl" paddingX={padSize} marginY="24" textAlign="left" alignSelf="start" _dark={{color: "white"}}>
       <Box marginBottom="5">
         <Text fontSize="3xl">👋About Me</Text>  
         <Box borderRadius='full' bg='cyan.600' width="44" marginBottom="3" height='0.5'><span></span></Box>
